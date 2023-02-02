a0d1cee087bda9d4cb247ef88d5d687e900824ed
William Gomez <89489831+WilliamG-1@users.noreply.github.com>
2023-02-02 11:20:52 -0800

    Basic html layout, script and css linked

---
+++ script.js
@@ -0,0 +1,20 @@
+function add (x, y) {
+    return x + y;
+}
+
+function subtract (x, y) {
+    return x - y;
+}
+
+function multiply (x, y) {
+    return x * y;
+}
+
+function divide (x, y) {
+    return x / y;
+}
+
+function operate (operation , x, y)
+{
+    return operation(x, y);
+}
