4044143e322113f071901c9bcffd66e83851b16e
William Gomez <89489831+WilliamG-1@users.noreply.github.com>
2023-02-02 19:49:44 -0800

    Function calculator working: Current bugs - Cannot operate on answer after calculation

--- script.js
+++ script.js
@@ -7,17 +7,24 @@ var operations = document.querySelectorAll(".operation");
 var numbers = document.querySelectorAll(".int_btn");
 var operationSelected = false;
 
-var x = 0; // First number
-var y = 0; // Second number
 var result = 0; // Result
-var operation = '';
+var calculated = true;
+var canOperate = true;
+function clear() {
+    result = 0;
+    textDisplay.textContent = 0;
+    operationDisplay.textContent = "";
+    calculated = true;
+};
 // Number button press
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
+        if (calculated) 
+            clear()
+        calculated = false;
         operationDisplay.textContent += number.textContent;
         operationSelected = false;
         result = parseInt(number.textContent);
-        textDisplay.textContent = result;
     });
 });
 // Oper button press
@@ -26,113 +33,39 @@ operations.forEach(function(operation) {
         if (!operationSelected)
         {
             operationDisplay.textContent += operation.textContent;
-            establish_operation(operation.textContent);
         }
         operationSelected = true;
     });
 })
 // Clear button press
-clearButton.addEventListener('click', () => {
-    result = 0;
-    textDisplay.textContent = 0;
-    operationDisplay.textContent = "";
+clearButton.addEventListener('click', clear);
+
+// Calculate Expression
+calculateButton.addEventListener('click', ()=>{
+    if (!calculated)
+    {
+        textDisplay.textContent = compute(operationDisplay.textContent);
+        operationDisplay.textContent = updateString(operationDisplay.textContent);
+        calculated = true;
+    }
 });
 
-function establish_operation(oper){
-    x = result;
-    operation = oper;
-}
-function add (x, y) {
-    return x + y;
-}
-
-function subtract (x, y) {
-    return x - y;
-}
-
-function multiply (x, y) {
-    return x * y;
-}
-
-function divide (x, y) {
-    return x / y;
-}
 
 function operate (operation , x, y)
 {
     switch(operation)
     {
         case '/':
-            return divide(x, y);
+            return x / y;
         case '×':
-            return multiply(x, y);
+            return x * y;
         case '−':
-            return subtract(x, y);
+            return x - y;
         case '+':
-            return add(x, y);
+            return x + y;
     }
 }
 
-function convertToOperation (string) {
-    
-    var x = 0;
-    var y = 0;
-    var result = 0;
-    var numberShouldBeMultiplied = false;
-
-    const multNumberArray = string.match(/\d+(?=×)|(?<=×)\d+|\d+(?=\/)|(?<=\/)\d+/g); // Get all numbers that either preceed or proceed a multiplication/division operator
-    const multOperationArray = string.match(/\/|×/g);
-    const addNumberArray = string.match(/\d+(?=\+)|(?<=\+)\d+|\d+(?=−)|(?<=−)\d+/g) // Get all numbers that either preceed or proceed an addition/subtraction operator
-    const addOperationArray = string.match(/−|\+/)
-    var currentNum = 0;
-    var decimalPlace = 1;
-    function reset() {
-        x = 0;
-        y = 0;
-        currentNum = 0;
-        decimalPlace = 1;
-    }
-
-
-    string.forEach((char)=>{
-        switch(char)
-        {
-            case '×':
-                multNumbers.push(currentNum);
-                numberShouldBeMultiplied = true; // Next number should be pushed into array for multiplication/divison
-                reset();
-                break;
-            case '+':
-                if (numberShouldBeMultiplied) {multNumbers.push(currentNum); numberShouldBeMultiplied = false;}
-                else {addNumbers.push(currentNum);}
-                reset();
-                break;
-            default:
-                currentNum += parseInt(char) * decimalPlace;
-                decimalPlace *= 10;
-                break;
-        }
-    });
-    return result;
-}
-
-function evaluateOperations(multArray, addArray){
-
-}
-
-// const stringg = "1+3+5×3+67×20/20/12";
-// console.log(stringg);
-// //const m = stringg.match(/\d+(?=\×)|(?<=\×)\d+/g);
-// const a = stringg.match(/\d+(?=×)|(?<=×)\d+|\d+(?=\/)|(?<=\/)\d+/g);
-// const b = stringg.match(/\/|×/g);
-// const c = stringg.match(/\d+(?=\+)|(?<=\+)\d+|\d+(?=−)|(?<=−)\d+/g); // Get all numbers that either preceed or proceed an addition/subtraction operator
-// const d = stringg.match(/−|\+/g)
-// console.log(a);
-// console.log(b);
-// console.log(c);
-// console.log(d);
-
-// console.log("--------------------------------------");
 function compute(string)
 {
     var prev = 0; // First number
@@ -141,7 +74,7 @@ function compute(string)
     var result = 0;
     var multPresentInArray = true;
     var operatorPresentInArray = true;
-    var arr = string.match(/\d+|./g);
+    var arr = string.match(/\d+|./g); // Seperates the string into an array of numbers and operations
     console.log(arr);
     
     // Do multiplications in array
@@ -159,7 +92,7 @@ function compute(string)
 
             prev = parseInt(arr[j]);
             cur = arr[j + 1]
-            next = parseInt(j < arr.length ? arr[j + 2] : 0);
+            next = parseInt(j + 2 < arr.length ? arr[j + 2] : 1);
 
             if (cur === "×" || cur === "/")
             {
@@ -187,7 +120,7 @@ function compute(string)
 
             prev = parseInt(arr[j]);
             cur = arr[j + 1]
-            next = parseInt(j < arr.length ? arr[j + 2] : 0);
+            next = parseInt(j + 2 < arr.length ? arr[j + 2] : 0);
 
             if (cur === "+" || cur === "−")
             {
@@ -201,7 +134,26 @@ function compute(string)
         console.log(arr);
         break;
     }
+    return arr[0];
 }
 
-compute("5+4×3×2/3×10+1+3+2+5×3");
+function updateString(string)
+{
+    const end = string.length - 1;
+    switch(string[end])
+    {
+        case '+':
+            return string.slice(0, end) + "=";
+        case '−':
+            return string.slice(0, end) + "=";
+        case '/':
+            return string.slice(0, end) + "=";
+        case '×':
+            return string.slice(0, end) + "=";
+        default:
+            return string + "=";
+    }
+}
+
+
 
